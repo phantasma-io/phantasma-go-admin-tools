523a74c1d8ffec265d35a8faa7bfd7e2d075ee30
alexmpa <alexander.paukonen@gmail.com>
2025-05-28 20:22:23 -0300

    SM Rewards: Move/update ignore address list

--- prepare-sm-rewards.sh
+++ prepare-sm-rewards.sh
@@ -1,7 +1,7 @@
 just build-account-analyzer
 
 ./bin/account-analyzer --get-sm-states --address-csv-path=./data/addresses.csv \
-  --ignore-address-csv-path=broken-addresses.csv \
+  --ignore-address-csv-path=./data/ignore-addresses.csv \
   --invalid-address-output-path=./data/invalid-addresses.csv \
   --errors-output-path=./data/errors.log \
   --export-sm-json=./data/sm-rewards.json --verbose
